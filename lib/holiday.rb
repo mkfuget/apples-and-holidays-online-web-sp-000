75e0b6c9750665fc8f9c7df0bcb2e5b4d127374c
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 06:04:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |key, value|
+    puts key
 end
 
 def all_holidays_with_bbq(holiday_hash)
