f27517ae510fbde80b24aee5608cf6c9de5b68be
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 06:26:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,8 +79,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-    holiday_hash.each do |key, value|
-      return 
+    out = [];
+    holiday_hash.values.each do |key, value|
+      if(value == "BBQ")
+        out.push(key)
 
 end
 
